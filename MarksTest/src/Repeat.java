73d4eee828a5822379fa70ea6176d67c556729a6
Alastair Campbell <alastair.campbell@outlook.com>
2014-02-23 21:36:37 +0000

    Reformatted a good deal of code and added a good deal of comments. Also changed switch statements to else..else if...else statements

--- MarksTest/src/Repeat.java
+++ MarksTest/src/Repeat.java
@@ -1,11 +1,15 @@
-public class Repeat{
-	public static boolean repeat(){
-	 	boolean result=false;
-		char response;
-		response=Input.getCharacter("continue(y/n)?  ");
-		if (response=='y')
-			result=true;
-		return result;
-	}
+public class Repeat
+{
+    public static boolean repeat()
+    {
+        boolean result = false;
+        char response;
+        response = Input.getCharacter("continue(y/n)?  ");
+        if (response == 'y')
+        {
+            result = true;
+        }
+        return result;
+    }
 }
 
